3435c13bcd0138a5715f15e038ae66783e6068b0
Claudia Hernandez <hernandez.clauds@gmail.com>
2018-07-09 21:05:51 -0400

    added notes

--- chapter2/script.js
+++ chapter2/script.js
@@ -2,6 +2,7 @@
 //define an addMessage Function that, when invoked, creates a new <li> 
 // element, sets its text content, and APPENDS to an existing element.
 
+//function code, in order to be executed, has to be called by something else. in this case, its "addMessage"
 function addMessage(element, message){
   var messageElement = document.createElement("li");
   messageElement.textContent = message;
@@ -11,6 +12,7 @@ function addMessage(element, message){
 // we follow this by using getElementByID method to fetch the element with the #ID #first
 // from the document and adding a messege to it that notifies us that the page is loading
 
+// global code, executed automatically
 var first = document.getElementById("first");
 addMessage(first, "Page is now loading");
 
