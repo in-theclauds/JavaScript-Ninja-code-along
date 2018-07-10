752102bf7be30319a072084604ce4b7846b85a23
Claudia Hernandez <hernandez.clauds@gmail.com>
2018-07-09 20:10:27 -0400

    setting up the problem

---
+++ chapter2/script.js
@@ -0,0 +1,15 @@
+
+//define an addMessage Function that, when invoked, creates a new <li> 
+// element, sets its text content, and APPENDS to an existing element.
+
+function addMessage(element, message){
+  var messageElement = document.createElement("li");
+  messageElement.textContent = message;
+  element.appendChild(messageElement);
+}
+
+// we follow this by using getElementByID method to fetch the element with the #ID #first
+// from the document and adding a messege to it that notifies us that the page is loading
+
+var first = document.getElementById("first");
+addMessage(first, "Page is now loading");
