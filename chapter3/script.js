6e41897d7dcbef45ff2cc74ebf65fbb7468d7bf4
Claudia Hernandez <hernandez.clauds@gmail.com>
2018-07-20 23:47:30 -0400

    simple callbacks

--- chapter3/script.js
+++ chapter3/script.js
@@ -1,60 +1,83 @@
-// Assign a new object to a variable
-var ninja = {};
+// // Assign a new object to a variable
+// var ninja = {};
 
-// Adding new object to an array
-ninjaArray.push ({});
+// // Adding new object to an array
+// ninjaArray.push ({});
 
-// Assigns a new object as a property of another obj.
-ninja.data = {};
+// // Assigns a new object as a property of another obj.
+// ninja.data = {};
 
-// Can be passed as arguments to functions -- newly created obj. passed as an argument to a function
-function hide(ninja) {
-  ninja.visibility = false;
+// // Can be passed as arguments to functions -- newly created obj. passed as an argument to a function
+// function hide(ninja) {
+//   ninja.visibility = false;
+// }
+
+// hide({});
+
+// // Can be returned as values from functions -- retuns a new onj from a function
+// function returnNewNinja() {
+//   return {};
+// }
+
+// // & can possess properties that are dynamically created and assigned -- creates a new property on an object
+
+// var ninja= {};
+// ninja.name = "Clauds";
+
+
+
+
+
+// // FUNCTIONS AS FIRST CLASS OBJECTS 
+
+
+// // We say funtions are first class objects, which can also be:
+
+// // Assigned to variables, aray entries, and properties of other objects
+
+// var ninjaFunction = function()  {};   -- // assigns a new function to a variable \\
+// ninjaArray.push(function(){}); // Adds a new function to an array\\
+// ninja.data = function() {}; //assigns a new function as a property of another object
+
+
+
+// // Passed as arguments to other functions
+// function call(ninjaFunction){
+//   ninjaFunction();
+// }
+// call (function() {}); //a newly created function passed as an argument to a function
+
+
+// // returned as values from functions 
+
+// function returnNewNinjaFunction() {
+//   return function() {} //returns new function
+// }
+
+
+// // They can also possess properties that are dynamically created and assigned
+// var ninjaFunction = function() {};
+// ninjaFunction.name = "clauds"
+
+
+// CHAPTER THREE CONTINUED
+
+// Simple callback ex.
+
+var text = "Domo arogato!";
+report("Before defining functions");
+
+function useless(ninjaCallback) {
+  report("In Useless function");
+  return ninjaCallback();   // defies a function tht takes a callback function and immediately invokes it
 }
 
-hide({});
-
-// Can be returned as values from functions -- retuns a new onj from a function
-function returnNewNinja() {
-  return {};
+function getText() {
+  report (" in getText function"); // defies a simple function that returns a global variable
+  return text;
 }
 
-// & can possess properties that are dynamically created and assigned -- creates a new property on an object
+assert (useless(getText) === text,
+"the useless function works! " + text);  // calls our useless function with the getText funct. as a callback
 
-var ninja= {};
-ninja.name = "Clauds";
-
-
-
-
-
-// FUNCTIONS AS FIRST CLASS OBJECTS 
-
-
-// We say funtions are first class objects, which can also be:
-
-// Assigned to variables, aray entries, and properties of other objects
-
-var ninjaFunction = function()  {};   -- // assigns a new function to a variable \\
-ninjaArray.push(function(){}); // Adds a new function to an array\\
-ninja.data = function() {}; //assigns a new function as a property of another object
-
-
-
-// Passed as arguments to other functions
-function call(ninjaFunction){
-  ninjaFunction();
-}
-call (function() {}); //a newly created function passed as an argument to a function
-
-
-// returned as values from functions 
-
-function returnNewNinjaFunction() {
-  return function() {} //returns new function
-}
-
-
-// They can also possess properties that are dynamically created and assigned
-var ninjaFunction = function() {};
-ninjaFunction.name = "clauds"
+report("After the calls have been made");
